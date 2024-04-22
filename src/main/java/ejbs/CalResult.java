7e8cd75cbe4e9a18b39fca39a66fff44e5622fa4
Amr-Emad-Eldin <moramoda99@gmail.com>
2024-04-23 01:00:12 +0200

    First commit

---
+++ src/main/java/ejbs/CalResult.java
@@ -0,0 +1,14 @@
+package ejbs;
+
+public class CalResult {
+	public CalResult(int r){
+		Result =r;
+	}
+	private int Result;
+	public int getResult() {
+		return Result;
+	}
+	public void setResult(int r) {
+		Result =r;
+	}
+}
